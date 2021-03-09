8264d146d5fbc3854325eb4becce4b3990aef12c
Kaaee <69994107+Kaaee@users.noreply.github.com>
2021-03-09 14:27:56 +0000

    Finished - No gui
    Works now - Currencies need to be in uppercase for it to work though.

--- main boi.py
+++ main boi.py
@@ -1,46 +1,42 @@
 # Needs pip for requests to be installed on the local machine
 # you still need to install requests in pycharm
-import requests
-
-# GUI Import
+from requests import get
 import tkinter
 
 
-# GUI (not done yet)
+# GUI
 
 
-class currency_converter:
+class CurrencyConverter:
 
     rates = {}
 
-    def __init__(self, url):
-        data = requests.get(url).json()
+    def __init__(self, web_url):
+        data = get(web_url).json()
 
         # we want base rates
         self.rates = data["rates"]
 
     # amount with conversion
-    def convert(self, from_currency, to_currency, amount):
-        initial_amount = amount
+    def convert(self, from_currency, to_currency, money):
+        initial_amount = money
         if from_currency != 'EUR':
-            amount = amount / self.rates[from_currency]
+            money = money / self.rates[from_currency]
 
-        amount = round(amount * self.rates[to_currency], 2)
-        print('{} {} = {} {}'.format(initial_amount, from_currency, amount, to_currency))
+        money = round(money * self.rates[to_currency], 2)
+        print('{} {} = {} {}'.format(initial_amount, from_currency, money, to_currency))
 
     # main
 
 
-
-
 if __name__ == "__main__":
-    print("Welcome to Currency Converter \n")
+    print("Welcome to Currency Converter, please only enter currency titles in uppercase. (GBP, USD, TRY) \n")
     print("Here are all the country codes: https://fixer.io/symbols\n")
     MY_ACCESS_KEY = "6b413894a1fc04d14cdf05ecc33d3fac"
     url = str.__add__('http://data.fixer.io/api/latest?access_key=', MY_ACCESS_KEY)
-    c = currency_converter(url)
+    c = CurrencyConverter(url)
     from_country = input("From Country: ")
-    to_country = input("TO Country: ")
-    amount = int(input("Amount: "))
+    to_country = input("To Country: ")
+    amount = int(input("Amount you'd like to convert: "))
 
     c.convert(from_country, to_country, amount)
